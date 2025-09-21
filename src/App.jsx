cf2bf232b822e4a2d121ebcc9f8b60b46f0266e6
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2025-09-21 17:30:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      
+      <h2>World's Countries Information using React</h2>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Country Lists in the World</h2>
+        </div>
+    );
+};
+
+export default Countries;
